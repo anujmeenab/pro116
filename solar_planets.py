80931a18e46d9b806393d91a90d310389428c2f1
anujmeenab <105282094+anujmeenab@users.noreply.github.com>
2023-01-04 19:46:42 +0530

    Add files via upload

---
+++ solar_planets.py
@@ -0,0 +1,17 @@
+import cv2
+img=cv2.imread("solar-system.jpg")
+cv2.putText(img,"sun",(20,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.putText(img,"mercury",(100,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.putText(img,"venus",(200,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.putText(img,"earth",(300,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.putText(img,"mars",(400,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.putText(img,"jupiter",(500,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.putText(img,"saturn",(800,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.putText(img,"uranus",(1000,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.putText(img,"neptune",(1100,300), cv2.FONT_HERSHEY_COMPLEX, 0.5,(255,255,255) )
+cv2.imshow("output",img)
+cv2.waitKey(0)
+cv2.imwrite("solarsystem.jpg")
+
+
+
